77bb684c403787836e03b54d4550be7ff2dc22c9
Tom Nielsen <tanielsen@gmail.com>
2025-02-12 11:00:45 +0000

    option to only allow logins from exisiting accounts

--- index.js
+++ index.js
@@ -20,13 +20,44 @@ const authentication = (config) => {
       icon: '<i class="fab fa-google"></i>',
       label: "Google",
       parameters: { scope: ["profile", "email"] },
-      strategy: new GoogleStrategy(
-        params,
-        function (accessToken, refreshToken, profile, cb) {
-          let email = "";
-          if (profile._json && profile._json.email) email = profile._json.email;
-          else if (profile.emails && profile.emails.length)
-            email = profile.emails[0].value;
+      strategy: new GoogleStrategy(params, function (
+        accessToken,
+        refreshToken,
+        profile,
+        cb
+      ) {
+        getState().log(6, "google-auth profile: " + JSON.stringify(profile));
+        let email = "";
+        if (profile._json && profile._json.email) email = profile._json.email;
+        else if (profile.emails && profile.emails.length)
+          email = profile.emails[0].value;
+        if (config.only_existing) {
+          // first try existing google id
+          User.findOne({
+            _attributes: { json: ["googleId", profile.id] },
+          }).then((mu) => {
+            if (mu?.disabled) return cb(null, false);
+            if (mu) return cb(null, u.session_object);
+
+            // no exisiting, check if it has email
+            User.findOne({ email }).then((mu) => {
+              if (!mu || mu.disabled) return cb(null, false);
+              if (
+                mu._attributes.googleId &&
+                mu._attributes.googleId !== profile.id
+              ) {
+                //there is a user but it has a different google id. Reject
+                return cb(null, false);
+              }
+              // set profile id & proceed
+              mu.update({
+                _attributes: { ...mu._attributes, googleId: profile.id },
+              }).then(() => {
+                return cb(null, mu.session_object);
+              });
+            });
+          });
+        } else {
           User.findOrCreateByAttribute("googleId", profile.id, {
             email,
           }).then((u) => {
@@ -34,7 +65,7 @@ const authentication = (config) => {
             return cb(null, u.session_object);
           });
         }
-      ),
+      }),
     },
   };
 };
@@ -47,7 +78,9 @@ const configuration_workflow = () => {
       : "",
     `Create a new application at the <a href="https://console.developers.google.com/apis/credentials">Google developer portal, API & Services, Credentials</a>. 
 you should obtain a OAuth 2.0 Client ID and secret, set the Authorised JavaScript origins to ${cfg_base_url}
-and set the Authorised redirect URI to ${ensure_final_slash(cfg_base_url)}auth/callback/google. HTTPS should be enabled.`,
+and set the Authorised redirect URI to ${ensure_final_slash(
+      cfg_base_url
+    )}auth/callback/google. HTTPS should be enabled.`,
   ];
   return new Workflow({
     steps: [
@@ -69,6 +102,13 @@ and set the Authorised redirect URI to ${ensure_final_slash(cfg_base_url)}auth/c
                 label: "Google Client Secret",
                 type: "String",
                 required: true,
+              },
+              {
+                name: "only_existing",
+                label: "Only existing",
+                sublabel:
+                  "Only allow logins from emails or Google IDs with existing user accounts",
+                type: "Bool",
               },
             ],
           }),
